c866cebd6eddee20762ce9e90fd028e9dfb0eb6e
zyxist <nightstorm@tlen.pl>
2010-03-24 13:51:44 +0000

    [trunk] Improved cooperation of FlashMessage with ZF sessions - part 2

--- lib/Invenzzia/View/Helper/FlashMessage.php
+++ lib/Invenzzia/View/Helper/FlashMessage.php
@@ -66,10 +66,12 @@ class Invenzzia_View_Helper_FlashMessage extends Invenzzia_View_Helper_Abstract
 		{
 			if(is_string($redirect))
 			{
+				Zend_Session::writeClose(false);
 				header('Location: '.$redirect);
 			}
 			elseif(is_array($redirect))
 			{
+				Zend_Session::writeClose(false);
 				$router = Zend_Controller_Front::getInstance()->getRouter();
 				if($routeName !== null)
 				{
